129b13dc784749f20636926e86b06c9f45eb23a9
Prudhvi Garapati <39909903+PrudhviGNV@users.noreply.github.com>
2020-08-13 16:32:06 +0530

    facial emotion recognizer deployment

---
+++ precache-manifest.11d9466ac15db0f25266f15ffc2fbf71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e58a4c4a6a9549fd62024722d44bb0",
+    "url": "/FacialEmotionDetector/index.html"
+  },
+  {
+    "revision": "72f05e15786ca72b64da",
+    "url": "/FacialEmotionDetector/static/css/main.996a38f0.chunk.css"
+  },
+  {
+    "revision": "4559cf55cf4b57a9c93a",
+    "url": "/FacialEmotionDetector/static/js/2.f748b66b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FacialEmotionDetector/static/js/2.f748b66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f05e15786ca72b64da",
+    "url": "/FacialEmotionDetector/static/js/main.e661599f.chunk.js"
+  },
+  {
+    "revision": "1fa662514ec3f7fc6e5b",
+    "url": "/FacialEmotionDetector/static/js/runtime-main.8d4161f2.js"
+  },
+  {
+    "revision": "5cf585fb2a38b17195dae89817db5b7f",
+    "url": "/FacialEmotionDetector/static/media/hero-bg.5cf585fb.png"
+  }
+]);
